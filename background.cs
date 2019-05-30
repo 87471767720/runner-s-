30c2b83d53b36faae0b53dcdd5e3d6b4d5e652ea
87471767720 <50128468+87471767720@users.noreply.github.com>
2019-05-30 18:35:54 +0500

    Add files via upload

---
+++ background.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        float heigth = Camera.main.orthographicSize * 2f;
+        float width = heigth * Screen.width / Screen.height;
+
+        transform.localScale = new Vector3(width, heigth, 0);
+
+        if (gameObject.name == "Background")
+        {
+            transform.localScale = new Vector3(width, heigth, 0);
+        }
+        else
+            transform.localScale = new Vector3(width, 2, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ playermovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour {
+
+    public float jumpForce = 5f;
+    public float forwardForce = 0f;
+    private Rigidbody2D myRB;
+    private bool canJump;
+
+	// Use this for initialization
+	void Start () {
+        myRB = GetComponent<Rigidbody2D> ();
+       
+
+	}
+
+    // Update is called once per frame
+    public void Jump()
+    {
+        if (canJump)
+
+        {
+            canJump = false;
+
+            if(transform.position.x < 0)
+            {
+                forwardForce = 1f;
+            }
+            else
+            {
+                forwardForce = 0f;
+            }
+            myRB.velocity = new Vector2(forwardForce, jumpForce);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        canJump = true;
+    }
+}
